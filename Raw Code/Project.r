7b83600e3fd5432ae269c3ad66e7798e686d02da
Bryan Willauer <bw4096@yahoo.com>
2020-04-25 09:45:40 -0400

    added some code

--- Raw Code/Project.r
+++ Raw Code/Project.r
@@ -1,2 +1,17 @@
 library(tidyverse)
 data("ToothGrowth")
+
+str(ToothGrowth)
+head(ToothGrowth)
+
+ToothGrowth %>% ggplot(aes(as.factor(dose),len, fill = supp)) +
+        facet_grid(.~ supp) +
+        geom_col() +
+        labs(x = "Dose", y = "Length")
+
+ToothGrowth %>% ggplot(aes(as.factor(dose),len, fill = supp)) +
+        facet_grid(.~ supp) +
+        geom_boxplot() +
+        labs(x = "Dose", y = "Length") +
+        ggtitle("Figure #1: Tooth Length By Dose Per Supplement")
+summary(ToothGrowth)
